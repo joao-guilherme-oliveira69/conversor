f284e5db6a3aad0520af1c386173f39ee4354198
joão guilherme de oliveira <canalputchola@gmail.com>
2024-09-18 22:37:07 -0300

    adicionado os cifrões

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ button.addEventListener("click",function(){
 
   switch(document.getElementById("coin_selector").value){
     case "dolar" : 
-      convert_coin.value = (real / 5.51).toFixed(2)
+      convert_coin.value = (real / 5.51).toFixed(2)+" $";
       break
 
     case "EUR" :
-      convert_coin.value = (real / 6.13).toFixed(2)
+      convert_coin.value = (real / 6.13).toFixed(2)+" €"   
       break 
 
     case "GBP" :
-      convert_coin.value =  (real / 7.27).toFixed(2)
+      convert_coin.value =  (real / 7.27).toFixed(2)+" £"
       break 
 
     case "JPY" :
-      convert_coin.value = (real / 0.039).toFixed(2)
+      convert_coin.value = (real / 0.039).toFixed(2)+" ¥"
       break 
   }
 })
