ef7fba4be1f07b73f026a5b54ee6368b92223bce
joão guilherme de oliveira <canalputchola@gmail.com>
2024-09-18 23:28:27 -0300

    integrassão com  a api

--- index.js
+++ index.js
@@ -3,22 +3,44 @@ const button = document.getElementById("convert_btn")
 button.addEventListener("click",function(){
   const real = document.getElementById("real").value
   const convert_coin = document.getElementById("convert_coin")
+  const selectedCoin = document.getElementById("coin_selector").value;
 
-  switch(document.getElementById("coin_selector").value){
-    case "dolar" : 
-      convert_coin.value = (real / 5.51).toFixed(2)+" $";
-      break
+  const apiKey = 'fe801a9bd1a9d3ad5ed42f67';
+  const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/BRL`;
 
-    case "EUR" :
-      convert_coin.value = (real / 6.13).toFixed(2)+" €"   
-      break 
+  
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      let rate;
+      
+      switch(selectedCoin) {
+        case "dolar":
+          rate = data.conversion_rates.USD;
+          convert_coin.value = (real * rate).toFixed(2) + " $";
+          break;
 
-    case "GBP" :
-      convert_coin.value =  (real / 7.27).toFixed(2)+" £"
-      break 
+        case "EUR":
+          rate = data.conversion_rates.EUR;
+          convert_coin.value = (real * rate).toFixed(2) + " €";
+          break;
 
-    case "JPY" :
-      convert_coin.value = (real / 0.039).toFixed(2)+" ¥"
-      break 
-  }
-})
+        case "GBP":
+          rate = data.conversion_rates.GBP;
+          convert_coin.value = (real * rate).toFixed(2) + " £";
+          break;
+
+        case "JPY":
+          rate = data.conversion_rates.JPY;
+          convert_coin.value = (real * rate).toFixed(2) + " ¥";
+          break;
+
+        default:
+          convert_coin.value = "Moeda não suportada.";
+      }
+    })
+    .catch(error => {
+      console.error('Erro ao obter as taxas de câmbio:', error);
+      convert_coin.value = "Erro ao obter taxas.";
+    });
+});
